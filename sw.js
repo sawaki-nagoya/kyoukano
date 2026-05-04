7ac4ae4ff7e7d4de2a522a60ab1ce253848e8be2
sawaki-nagoya <ren.konno0924@i.softbank.jp>
2026-05-04 18:04:21 +0900

    Fix: add auto cache clear to sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,23 @@
+const CACHE_VERSION = 'v2';
+
+self.addEventListener('install', function(event) {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    }).then(function() {
+      return self.clients.claim();
+    })
+  );
+});
+
 self.addEventListener('push', function(event) {
   const data = event.data ? event.data.json() : {};
   const title = data.title || 'KYOUKANO';
